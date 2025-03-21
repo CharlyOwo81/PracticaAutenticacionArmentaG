96c8f9119420c7866d3d73f88265871a3b26d145
CharlyOwo81 <gamaliel.armenta245254@potros.itson.edu.mx>
2025-03-20 18:59:06 -0700

    Lógica implementada

--- app/src/main/java/mx/edu/itson/practicaautenticacionarmentag/MainActivity.kt
+++ app/src/main/java/mx/edu/itson/practicaautenticacionarmentag/MainActivity.kt
@@ -1,10 +1,16 @@
 package mx.edu.itson.practicaautenticacionarmentag
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.ktx.app
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +18,16 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val email = intent.extras!!.getString("user")
+
+        val user : TextView = findViewById(R.id.tvUser)
+        val button: Button = findViewById(R.id.btnLogout)
+
+        button.setOnClickListener {
+            Firebase.auth.signOut()
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
     }
 }
